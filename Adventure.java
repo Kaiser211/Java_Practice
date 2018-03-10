12b2d4113be95855ffa858720b30d225b3757b65
Kashaer <kaiser.kaiser211@gmail.com>
2018-03-10 06:58:29 -0500

    initial commit

---
+++ Adventure.java
@@ -0,0 +1,28 @@
+package com.assignmentsjavaoopadvance;
+
+import java.util.Scanner;
+
+public class Adventure{
+	private Scanner input;
+	private String text;
+
+	public Adventure(){
+		input = new Scanner(System.in);
+		text = "";
+
+		while(!text.equals("quit")){
+			text = input.nextLine();
+
+			if(text.equals("left")){
+				System.out.println("YOU WENT LEFT DOWN THE DARK SCARY ROAD.");
+			}else if(text.equals("right")){
+				System.out.println("YOU RAN INTO A TREE, GAME OVER!");
+				text = "quit";
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		new Adventure();
+	}
+}
